cdb03168c52b28136f9e71cb99a8207117588d52
Brenda Moreira <brendanatalia94@hotmail.com>
2020-12-30 15:15:43 +0100

    Add genre on venue model and form submit

--- app.py
+++ app.py
@@ -39,6 +39,7 @@ class Venue(db.Model):
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     address = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
@@ -250,6 +251,7 @@ def create_venue_submission():
       city=data['city'],
       state=data['state'],
       address=data['address'],
+      genres=data['genres'],
       phone=data['phone'],
       image_link=data['image_link'],
       facebook_link=data['facebook_link'],

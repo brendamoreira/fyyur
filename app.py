138b2b938c32005081cce101e40462540ee11641
Brenda Moreira <brendanatalia94@hotmail.com>
2021-01-08 13:57:49 +0100

    Add linebreaks on flask import

--- app.py
+++ app.py
@@ -6,7 +6,15 @@ import json
 import dateutil.parser
 import babel
 from datetime import datetime
-from flask import Flask, render_template, request, Response, flash, redirect, url_for
+from flask import (
+  Flask,
+  render_template,
+  request,
+  Response,
+  flash,
+  redirect,
+  url_for
+)
 from flask_migrate import Migrate
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
